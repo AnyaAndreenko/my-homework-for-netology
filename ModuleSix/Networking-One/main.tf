867f65854bef1522151cdc21dbd52faf62c1c671
AnyaAndreenko <91888428+AnyaAndreenko@users.noreply.github.com>
2025-06-21 15:09:54 +0300

    Create main.tf

---
+++ ModuleSix/Networking-One/main.tf
@@ -0,0 +1,106 @@
+resource "yandex_vpc_network" "main" {
+  name = "main-network"
+}
+
+resource "yandex_vpc_subnet" "public" {
+  name           = "public-subnet"
+  zone           = var.yc_zone
+  network_id     = yandex_vpc_network.main.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_compute_instance" "nat" {
+  name        = "nat-instance"
+  platform_id = "standard-v1"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id  = yandex_vpc_subnet.public.id
+    ip_address = "192.168.10.254"
+    nat        = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "public_vm" {
+  name        = "public-vm"
+  platform_id = "standard-v1"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+resource "yandex_vpc_route_table" "private_route" {
+  name       = "private-route"
+  network_id = yandex_vpc_network.main.id
+
+  static_route {
+    destination_prefix = "0.0.0.0/0"
+    next_hop_address   = "192.168.10.254"
+  }
+}
+
+resource "yandex_vpc_subnet" "private" {
+  name           = "private-subnet"
+  zone           = var.yc_zone
+  network_id     = yandex_vpc_network.main.id
+  route_table_id = yandex_vpc_route_table.private_route.id
+  v4_cidr_blocks = ["192.168.20.0/24"]
+}
+
+resource "yandex_compute_instance" "private_vm" {
+  name        = "private-vm"
+  platform_id = "standard-v1"
+  zone        = var.yc_zone
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.private.id
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
