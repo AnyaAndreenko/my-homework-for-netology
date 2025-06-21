7e80133af44128933daaaa17297d2cb8c6369ad7
AnyaAndreenko <91888428+AnyaAndreenko@users.noreply.github.com>
2025-06-21 15:08:44 +0300

    Create variables.tf

---
+++ ModuleSix/Networking-One/variables.tf
@@ -0,0 +1,19 @@
+variable "folder_id" {
+  default = ""
+}
+
+variable "cloud_id" {
+  default = ""
+}
+
+variable "yandex_token" {
+  default = ""
+}
+
+variable "ssh_public_key" {
+  default = "~/.ssh/id_ed25519.pub"
+}
+
+variable "ssh_user" {
+  default = ""
+}
